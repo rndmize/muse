0db2b765246ed4993d3fd24d81e328e653011f41
Hack Reactor Students <github@hackreactor.com>
2014-04-01 13:51:42 -0700

    Stuck on log component

--- client/app.js
+++ client/app.js
@@ -3,12 +3,14 @@ angular.module('muse', [
   'ngRoute'
 ]);
 var muse = angular.module('muse');
+angular.module('muse').constant('loginRedirectPath', '/login');
 
 muse.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/login', {
-        templateUrl: 'templates/login.html'
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
       }).
       when('/stats', {
         templateUrl: 'templates/stats.html',
@@ -18,19 +20,38 @@ muse.config(['$routeProvider',
         templateUrl: 'templates/gm.html'
       }).
       otherwise({
-        redirectTo: '/'
+        redirectTo: '/login'
       });
   }
 ]);
 
 muse.factory('fbService', ['$firebase', function($firebase) {
-  var ref = new Firebase('https://sweltering-fire-1770.firebaseio.com/character/baseStats');
+  var ref = new Firebase('https://sweltering-fire-1770.firebaseio.com/character');
   return $firebase(ref);
 }]);
 
 muse.controller('BaseStatsCtrl', ['$scope', 'fbService',
   function($scope, service) {
+    //$scope.baseStats = {};
     service.$bind($scope, 'baseStats');
     console.log($scope);
+    // $scope.$on('change', function() {
+    //   console.log('change');
+    // });
+  }
+]);
+
+muse.controller('LoginCtrl', ['$scope', '$firebaseSimpleLogin',
+  function($scope, $firebaseSimpleLogin) {
+    var dataRef = new Firebase('https://sweltering-fire-1770.firebaseio.com');
+    $scope.loginObj = $firebaseSimpleLogin(dataRef);
+
+  }
+]);
+
+muse.controller('LogCtrl', ['$scope', 'fbService',
+  function($scope) {
+    // $scope.$on('change', function() {
+    // });
   }
 ]);
